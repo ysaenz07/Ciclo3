c31adead546827d5391c2d433b1599bc9fd70ad3
ysaenz07 <90258850+ysaenz07@users.noreply.github.com>
2021-11-14 17:17:10 -0500

    Delete front directory

--- front/src/components/ImagenLogoP.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Logo from 'media/logo.png';
-
-const ImagenLogoP = () => {
-  return <img className='mx-auto h-20 w-auto' src={Logo} alt='Workflow' />;
-};
-
-export default ImagenLogoP;
